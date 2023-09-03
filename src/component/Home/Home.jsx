5d0de76730ed18cb6988b0540c3cb9cbdc6b9576
hasibprangon <hasibulhossainp09@gmail.com>
2023-09-03 21:37:22 +0600

    geting data from json file

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
+    const tShirts = useLoaderData()
     return (
         <div>
-            <h2 className="text-3xl font-bold">this is home</h2>
+            <h2 className="text-3xl font-bold">tShirt: {tShirts.length}</h2>
         </div>
     );
 };
